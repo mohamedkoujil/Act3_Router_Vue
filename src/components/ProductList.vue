77ab7e64e639ebaa0369320312d17590e496ee64
Mohamed Koujil <mohamed.koujil.7e7@itb.cat>
2024-11-19 18:49:31 +0100

    ProductList component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import products from '../assets/productos.json'
+</script>
+
+<template>
+  <ul>
+    <li v-for="product in products" :key="product.id">
+      <RouterLink :to="'/products/' + product.id">
+        {{ product.name }}
+      </RouterLink>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,17 +1,10 @@
 <script setup>
-import { RouterLink } from 'vue-router'
-import products from '../assets/productos.json'
+import ProductList from '@/components/ProductList.vue'
 </script>
 
 <template>
   <h1>Products</h1>
-  <ul>
-    <li v-for="product in products" :key="product.id">
-      <RouterLink :to="'/products/' + product.id">
-        {{ product.name }}
-      </RouterLink>
-    </li>
-  </ul>
+  <ProductList />
 </template>
 
 <style scoped></style>
